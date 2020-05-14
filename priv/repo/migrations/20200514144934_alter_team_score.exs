c0c89fd8897791415a98aa7981cf0c3527e32ef9
ldd <leonardoldd@gmail.com>
2020-05-14 14:23:26 -0400

    fix: autofix wrongly formatted files

--- priv/repo/migrations/20200514144934_alter_team_score.exs
+++ priv/repo/migrations/20200514144934_alter_team_score.exs
@@ -5,7 +5,7 @@ defmodule HnCommentsGame.Repo.Migrations.AlterTeamTableScore do
     alter table(:teams) do
       add :correct_questions, :integer, default: 0
       add :answered_questions, :integer, default: 0
-      remove_if_exists :score, :integer
+      remove_if_exists(:score, :integer)
     end
   end
 end
